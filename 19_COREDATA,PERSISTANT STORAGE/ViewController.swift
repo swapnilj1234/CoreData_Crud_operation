3ea7a449fa3fc457cb5eae7e2822fd444223de03
swapnil <swap81733@gmail.com>
2019-01-01 07:40:49 +0530

    Edit on ->   LeadingSwipeActionConfigurationRowAt Delete On-> TrailingSwipeActionConfigurationRowAt

--- 19_COREDATA,PERSISTANT STORAGE/ViewController.swift
+++ 19_COREDATA,PERSISTANT STORAGE/ViewController.swift
@@ -22,7 +22,7 @@ class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource
         
         loadData()
         
-        print(try! FileManager.default.urls(for: .documentDirectory, in: .userDomainMask))
+        print(try! FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: false))
           
        }
     
@@ -110,16 +110,71 @@ class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        
-        
-        context.delete(items[indexPath.row])
-        items.remove(at:indexPath.row)
-        
-       // items[indexPath.row].done = !items[indexPath.row].done
-        
+
+
+       // context.delete(items[indexPath.row])
+        //items.remove(at:indexPath.row)
+
+        items[indexPath.row].done = !items[indexPath.row].done
+
         saveData()
-        
+
         tables.deselectRow(at: indexPath, animated: true)
+    }
+    
+    func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+
+
+        let deleteAction = UIContextualAction(style: .normal, title: "Delete") { (action, view, handler) in
+            
+
+            self.context.delete(self.items[indexPath.row])
+            self.items.remove(at: indexPath.row)
+            
+            self.saveData()
+            
+        }
+        
+        deleteAction.backgroundColor = .red
+        let config = UISwipeActionsConfiguration(actions: [deleteAction])
+        
+        return config
+    }
+    func tableView(_ tableView: UITableView, leadingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+        
+        var textfiled = UITextField()
+        
+        let editAction = UIContextualAction(style: .normal,title: "Edit") { (action , view, handler) in
+            
+         
+            let alert = UIAlertController(title: "Edit data", message: "Write a new name", preferredStyle: .alert)
+        
+            let action = UIAlertAction(title: "Ok", style: .default) { (action) in
+            
+                self.items[indexPath.row].title = textfiled.text
+                
+                self.saveData()
+                
+                
+                    }
+            alert.addTextField { (alerttext) in
+                
+
+                alerttext.text = self.items[indexPath.row].title
+                
+                textfiled = alerttext
+                 
+            }
+            
+         
+            alert.addAction(action)
+            self.present(alert, animated: true, completion: nil)
+        }
+
+        editAction.backgroundColor = .green
+        let config = UISwipeActionsConfiguration(actions: [editAction])
+        
+        return config
     }
 }
 
